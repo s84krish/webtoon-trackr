8e10664cc5d9a2786d6b437443e811622bea7c0a
s84krish <sibimkrishnan@gmail.com>
2023-02-10 18:29:37 -0500

    Add links to chrome storage from context menu

--- src/background.js
+++ src/background.js
@@ -1,16 +1,41 @@
 chrome.runtime.onInstalled.addListener(() => {
   console.log('IT WORKS!?')
 
+
   //create context menu
   chrome.contextMenus.create({
     id: 'tracker',
     title: 'Add to Trackr',
     contexts: ['link']
   });
+
+
+  chrome.storage.sync.get(["list"]).then((result) => {
+    if (result.list == null){
+      const list = new Array();
+      chrome.storage.sync.set({list:list}).then(() => {
+        console.log("created list");
+      });
+    }
+    console.log(result.list)
+  });
   
+  chrome.contextMenus.onClicked.addListener(function(info){
+    console.log(info.linkUrl);
+    
+    chrome.storage.sync.get({list: []}).then((result) => {
+      const newList = result.list;
+      if (newList && Array.isArray(newList)){
+        newList.push(info.linkUrl);
+      }
+      chrome.storage.sync.set({ list: newList }).then(() => {
+        for (let i=0; i < newList.length; i++){
+          console.log("List " + i + " " + newList[i]);
+        }
+      });
+    });
+  });
+  
+
 });
 
-
-chrome.contextMenus.onClicked.addListener(function(info, tab){
-  console.log(info)
-});
