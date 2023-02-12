395c79767e245b1a91631e4de4f3207db6edeffb
s84krish <sibimkrishnan@gmail.com>
2023-02-11 21:29:53 -0500

    Clear and remove individual items

--- src/App.js
+++ src/App.js
@@ -1,23 +1,49 @@
+import React, { useState, useEffect } from 'react';
+
+import List from './components/List'
 import logo from './logo.svg';
+
 import './App.css';
 
-function App() {
+
+
+const clearStorage = () => {
+  chrome.storage.sync.clear();
+};
+
+// const removeItem = (index, data) => {
+//   data?.splice(index, 1);
+//   chrome.storage.sync.set({ list: data });
+// }
+
+
+
+const App = () => {
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    chrome.storage.sync.get(["list"]).then((list) => {
+      setData(list);
+    });
+  }, [data])
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Webtoon TRACKER.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <form onSubmit={clearStorage}>
+          <button>Clear</button>
+        </form>
+        <List data={data?.list}/>
+        {/* <ul>
+          {data.list?.map((item, index) => {
+            return (
+              <li key={index}>
+                {item}
+                <button onClick={() => removeItem(index, data.list)}>x</button>
+              </li>
+            )
+          })}
+        </ul> */}
     </div>
   );
 }
---
+++ src/components/List.js
@@ -0,0 +1,24 @@
+import React from 'react';
+
+const List = ({data}) => {
+
+  const removeItem = (index, data) => {
+    data?.splice(index, 1);
+    chrome.storage.sync.set({ list: data });
+  }
+  
+  return (
+    <ul>
+      {data?.map((item, index) => {
+        return (
+          <li key={index}>
+            {item}
+            <button onClick={() => removeItem(index, data)}>x</button>
+          </li>
+        )
+      })}
+    </ul>
+  )
+}
+
+export default List
